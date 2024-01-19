4bc6cf2ab43d563d444bc954348e014d6709be24
bill-sparkx <bernardbill316@gmail.com>
2024-01-18 20:41:43 -0800

    LIFo

--- main.c
+++ main.c
@@ -1,57 +1,83 @@
 #include "monty.h"
-
-bus_t bus = {NULL, NULL, NULL, 0};
+stack_t *head = NULL;
 
 /**
- * process_file - Processes the content of the Monty file.
- * @file_path: Path to the Monty file
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
-void process_file(const char *file_path) {
-    FILE *file = fopen(file_path, "r");
-    bus.file = file;
 
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", file_path);
-        exit(EXIT_FAILURE);
-    }
-
-    size_t size = 0;
-    ssize_t read_line = 1;
-    char *content = NULL;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
-
-    while (read_line > 0) {
-        content = NULL;
-        read_line = getline(&content, &size, file);
-        bus.content = content;
-        counter++;
-
-        if (read_line > 0) {
-            execute(content, &stack, counter, file);
-        }
-
-        free(content);
-    }
-
-    free_stack(stack);
-    fclose(file);
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
 }
 
 /**
- * main - Monty code interpreter
- * @argc: Number of arguments
- * @argv: Monty file location
- * Return: 0 on success
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
  */
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+stack_t *create_node(int n)
+{
+	stack_t *node;
 
-    process_file(argv[1]);
-
-    return 0;
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
 }
 
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
